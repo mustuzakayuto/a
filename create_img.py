33ee5da8a1f5f1f890d861a0387cf155b728d243
松坂侑隼 <220063ie@yse-c.net>
2023-12-04 10:47:02 +0900

    変更

---
+++ create_img.py
@@ -0,0 +1,54 @@
+import io
+import os
+
+import modal
+
+stub = modal.Stub()
+
+
+@stub.function(
+    image=modal.Image.debian_slim().pip_install("torch", "diffusers[torch]", "transformers", "ftfy",'uvicorn==0.21.1','pyOpenSSL==23.1.1','numpy==1.23.5','torch==2.0.1','torchaudio==2.0.2','resampy==0.4.2','python-socketio==5.8.0','fastapi==0.95.1','python-multipart==0.0.6','onnxruntime-gpu==1.13.1','scipy==1.10.1','matplotlib==3.7.1','websockets==11.0.2','faiss-cpu==1.7.3','torchcrepe==0.0.18','librosa==0.9.1','gin==0.1.6','gin_config==0.5.0','einops==0.6.0','local_attention==1.8.5','websockets==11.0.2','sounddevice==0.4.6','dataclasses_json==0.5.7','onnxsim==0.4.28',"GitPython","pyngrok")
+    .apt_install(
+        "git",
+        "python3-pyaudio"
+    )
+    .run_commands(
+        "git clone https://github.com/w-okada/voice-changer.git",
+        
+        
+    ),
+    secret=modal.Secret.from_name("my-huggingface-secret"),
+    
+    
+    gpu="any",
+)
+async def run_stable_diffusion():
+    
+    
+    
+    import os
+    
+    os.chdir("./")
+    path = os.getcwd()
+    import glob
+
+    files = glob.glob("./")
+    for file in files:
+        print(file)
+    print(path)
+    os.chdir("/voice-changer/server")
+    port = 18888
+    from pyngrok import ngrok, conf
+    # accesstoken
+    conf.get_default().auth_token = ""
+    # domain
+    public_url = ngrok.connect(port,hostname="")
+    print(f"ngrok URL: {public_url}")
+    
+    import subprocess
+    subprocess.run(["python", "MMVCServerSIO.py"])
+   
+
+@stub.local_entrypoint()
+def main():
+    img_bytes = run_stable_diffusion.remote()
