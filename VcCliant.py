68675dd55dc6337942a90404db3b43c16764f486
matuzaka_yuuto <120072340+mustuzakayuto@users.noreply.github.com>
2024-01-22 10:57:30 +0900

    Update VcCliant.py

--- VcCliant.py
+++ VcCliant.py
@@ -7,7 +7,7 @@ stub = modal.Stub()
 
 
 @stub.function(
-    image=modal.Image.debian_slim().pip_install("torch", "diffusers[torch]", "transformers", "ftfy",'uvicorn==0.21.1','pyOpenSSL==23.1.1','numpy==1.23.5','torch==2.0.1','torchaudio==2.0.2','resampy==0.4.2','python-socketio==5.8.0','fastapi==0.95.1','python-multipart==0.0.6','onnxruntime-gpu==1.13.1','scipy==1.10.1','matplotlib==3.7.1','websockets==11.0.2','faiss-cpu==1.7.3','torchcrepe==0.0.18','librosa==0.9.1','gin==0.1.6','gin_config==0.5.0','einops==0.6.0','local_attention==1.8.5','websockets==11.0.2','sounddevice==0.4.6','dataclasses_json==0.5.7','onnxsim==0.4.28',"GitPython","pyngrok")
+    image=modal.Image.debian_slim().pip_install("torch", "diffusers[torch]", "transformers", "ftfy",'uvicorn==0.21.1','pyOpenSSL==23.1.1','numpy==1.23.5','torch==2.0.1','torchaudio==2.0.2','resampy==0.4.2','python-socketio==5.8.0','fastapi==0.95.1','python-multipart==0.0.6','onnxruntime-gpu==1.13.1','scipy==1.10.1','matplotlib==3.7.1','websockets==11.0.2','faiss-cpu==1.7.3','torchcrepe==0.0.18','librosa==0.9.1','gin==0.1.6','gin_config==0.5.0','einops==0.6.0','local_attention==1.8.5','websockets==11.0.2','sounddevice==0.4.6','dataclasses_json==0.5.7','onnxsim==0.4.28',"GitPython","pyngrok","fairseq","pyworld")
     .apt_install(
         "git",
         "python3-pyaudio"
